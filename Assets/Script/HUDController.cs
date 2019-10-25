104d436434ae8da5365769a31db911f3d0729f2e
Kevin Andres <kevinagredo13@gmail.com>
2019-10-25 15:14:50 -0400

    Added Project

---
+++ Assets/Script/HUDController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class HUDController : MonoBehaviour
+{
+    public Text hungerText;
+    public Text friendshipText;
+    public Text isSickText;
+
+    public GameObject Rock;
+    // Start is called before the first frame update
+    void Start()
+    {
+       /* hungerText.text = "I'm hungry";
+        friendshipText.text = "Hello";
+        isSickText.text = "Healthy"; */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hungerText.text = Rock.GetComponent<RockController>().hunger.ToString();
+        friendshipText.text = Rock.GetComponent<RockController>().friendship.ToString();
+        isSickText.text = Rock.GetComponent<RockController>().isSick.ToString();
+    }
+
+   public void FeedRock()
+    {
+        Rock.GetComponent<RockController>().hunger += 2;
+    }
+    public void playwithrock()
+    {
+        Rock.GetComponent<RockController>().friendship += 2;
+    }
+    public void Vet()
+    {
+        Rock.GetComponent<RockController>().isSick = false;
+    }
+}
---
+++ Assets/Script/RockController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RockController : MonoBehaviour
+{
+ 
+
+    public string Rockname = "rocky";
+    public float hunger = 100f;
+    public float friendship = 100f;
+    public bool isSick = false;
+
+    float hungerScale = 2f;
+    float friendScale = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Rockname);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StatusUpdate();
+     
+    }
+    void StatusUpdate()
+    {
+        hunger -= Time.deltaTime * hungerScale;
+        friendship -= Time.deltaTime * friendScale;
+
+        int sickChance = Random.Range(0, 100);
+        if (sickChance ==1)
+        {
+            isSick = true;
+        }
+        if (isSick = false)
+        {
+            hungerScale = 2f;
+            friendScale = 2f;
+        }
+        else
+        {
+            hungerScale = 5f;
+            friendScale = 5f;
+        }
+        
+    }
+    void IsDead()
+    {
+        if (hunger <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else if (friendship <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+  
+}
